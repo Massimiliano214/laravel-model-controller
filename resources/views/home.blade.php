32de26a5fae7332f904ac4b9f3c5e4fa2ab3012e
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-11 16:13:31 +0200

    added cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,27 @@
 @section('title', 'Film List')
 
 @section('content')
-    <h1>Elenco Films</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            
-            <li>{{$movie->title}}</li>
+<div class="container my-3">
+    <h1>Films List</h1>
+    <div class="row">
 
-        @endforeach
-    </ul>
+        <div class="wrapper d-flex flex-wrap justify-content-between">
+
+            @foreach ($movies as $movie)
+                    
+                <div class="card col-3 my-4" style="width: 18rem;">
+                    <div class="card-body">
+                        <h4 class="card-title">Title: {{$movie->title}}</h4>
+                        <h5 class="card-text">Original Title: {{$movie->original_title}}</h5>
+                        <p class="card-text">Score: {{$movie->vote}}</p>
+                        <p class="card-text">Nationality: {{$movie->nationality}}</p>
+                        <p class="card-text">Release Date: {{$movie->date}}</p>
+                    </div>
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
+</div>
 @endsection
